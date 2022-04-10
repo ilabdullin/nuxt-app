ad20b2fb40a6bb79d6def3c244afec4aa4982a2c
ilabdullin <ilnurabdullin578@gmail.com>
2022-04-10 20:20:27 +0500

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <h1>About page</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati in
+      tenetur maxime! Delectus debitis saepe, adipisci laboriosam asperiores
+      voluptatum!
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur
+      quisquam minima maiores. Officia, earum doloremque consectetur possimus
+      atque deserunt soluta accusantium placeat quia necessitatibus dolor.
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa suscipit
+      voluptatum inventore perspiciatis? Quod, quas!
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  middleware: ['auth'],
+};
+</script>
+
+<style></style>
